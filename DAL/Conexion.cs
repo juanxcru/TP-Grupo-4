c929fa03c045843704da19be9b782426bbe6c21f
Gsotelo2022 <100044399+Gsotelo2022@users.noreply.github.com>
2023-06-11 21:05:50 -0300

    Comento cadena de conexion

--- DAL/Conexion.cs
+++ DAL/Conexion.cs
@@ -24,7 +24,7 @@ namespace DAL
         private void Conectar()
         {   // HACK: Cadena de conexión hardcodeada. Luego ponerla como parametro de configuración del proyecto u otra alternativa.
             //strCadenaDeConexion = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=TiendaWeb;Data Source=.\\SQLEXPRESS;";
-            strCadenaDeConexion = "Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = TiendaWeb; Data Source = GSOTELO\\SQLEXPRESS01";            
+            //strCadenaDeConexion = "Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = TiendaWeb; Data Source = GSOTELO\\SQLEXPRESS01";            
 
             //Instanció un objeto del tipo SqlConnection
             objConexion = new SqlConnection();
